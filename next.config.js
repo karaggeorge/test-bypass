e39b2de71435e2adcd1e36b17481779f01cf9433
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2022-12-12 12:18:36 -0500

    Test headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'x-test-me',
+            value: 'works',
+          },
+          {
+            key: 'content-type',
+            value: '*/*',
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
